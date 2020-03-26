07dab2e9923da6d07d442613af46586d632635be
hwoj <hwojnicki41@gmail.com>
2020-03-26 11:26:59 -0400

    yeet skeet

---
+++ src/main/java/com/example/SMCSLearn/Question.java
@@ -0,0 +1,12 @@
+package com.example.SMCSLearn;
+
+public class Question {
+    private String question;
+    public Question(String question){
+        this.question  = question;
+    }
+
+    public String getQuestion(){
+        return this.question;
+    }
+}
