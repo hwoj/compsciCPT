5976d357ef99dc091eb2c2760b35b8c22652ac7c
voz <pjchuchra@gmail.com>
2020-04-20 11:16:06 -0400

    update user class

--- src/main/java/com/example/SMCSLearn/User.java
+++ src/main/java/com/example/SMCSLearn/User.java
@@ -5,11 +5,19 @@ public class User {
     private String email;
     private String fname;
     private String lname;
-    public User(int id, String fname , String lname, String email){
+    private int crit_score;
+    private int eager_score;
+    private int agree_score;
+    private int extro_score;
+    public User(int id, String fname , String lname, String email, int crit_score, int eager_score, int agree_score, int extro_score){
         this.id = id;
         this.fname = fname;
         this.lname = lname;
         this.email = email;
+        this.crit_score = crit_score;
+        this.eager_score = eager_score;
+        this.agree_score = agree_score;
+        this.extro_score = extro_score;
     }
 
     public int getId(){
@@ -27,4 +35,12 @@ public class User {
     public String getLname() {
         return lname;
     }
+
+    public int getCrit_score() { return crit_score; }
+
+    public int getEager_score() { return eager_score; }
+
+    public int getAgree_score() { return agree_score; }
+
+    public int getExtro_score() { return extro_score; }
 }
