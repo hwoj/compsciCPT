85f6f6f32a304eae56b1e6ba9c1fc9d6eee3865a
hwoj <hwojnicki41@gmail.com>
2020-04-30 12:03:28 -0400

    now writes to JSON file for student

--- src/main/java/com/example/SMCSLearn/Auth.java
+++ src/main/java/com/example/SMCSLearn/Auth.java
@@ -1,14 +1,35 @@
 package com.example.SMCSLearn;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.*;
+
 @RestController
 public class Auth {
     @PostMapping(path = "/signup/student", consumes = "application/json")
-    public void registerStudent(@RequestBody User student){
+    public User registerStudent(@RequestBody User student) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+
+
+        String jsonString = mapper.writeValueAsString(student);
+        try {
+            File file = new File(getClass().getResource("/json/students.json").getFile());
+            FileOutputStream outputStream = new FileOutputStream(file);
+            byte[] strBytes = jsonString.getBytes();
+            outputStream.write(strBytes);
+            outputStream.close();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+        return student;
 
     }
 }
